3266e81bcd88f3b3a4ec31413c37f8eda82e0b3f
BhawanaMital <you@example.com>
2019-08-28 14:15:05 +0530

    search product using spring

--- src/main/java/com/stackroute/repository/TrackRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stackroute.repository;
-
-import com.stackroute.domain.Track;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrackRepository extends JpaRepository<Track,Integer> {
-
-}
